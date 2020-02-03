57b3ca196bb6ac11db711d40b0fa51c1ea9ca44a
Ekin Katkat <ekinkatkat_1996@hotmail.com>
2020-02-03 14:20:40 +0300

    Model katmanı oluşturuldu.

---
+++ Project.MODEL/Entities/AppUserDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.MODEL.Entities
+{
+    public class AppUserDetail:BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //Relational Properties begin...
+
+        public virtual AppUser AppUser { get; set; }
+
+    }
+}
---
+++ Project.MODEL/Entities/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.MODEL.Entities
+{
+    public class Category:BaseEntity
+    {
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+
+        //Relational Properties begin...
+
+
+        public virtual List<Product> Products { get; set; }
+    }
+}
